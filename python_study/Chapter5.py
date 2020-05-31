a010bf2d32ec5b2f70c17b15a9e0454451f38faa
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-05-31 17:39:31 +0900

    Chapter5-1

--- python_study/Chapter5.py
+++ python_study/Chapter5.py
@@ -1,2 +1,11 @@
-print("test")
-print("復習")
+#数当てゲームを作る
+#「ヒット＆ブロー」
+
+
+#ランダムな1桁の数を表示する
+#coding : utf-8
+import random #randomモジュールインポート
+
+a = random.randint(0 , 9) #0から9までのランダムな値を作る、実行するたびに結果が変わる
+print(a)
+
