b519d65f4c2f8e346f2ef0439d8d11fe6060a3d2
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-05 12:07:01 +0900

    python_study7-7途中

--- python_study7/python_study7-6.py
+++ python_study7/python_study7-6.py
@@ -70,7 +70,7 @@ root = tk.Tk()
 root.geometry("600x400")
 
 # キャンバスを置く
-canvas =tk.Canvas(root, width =600, height =400, bg="white")
+canvas =tk.Canvas(root, width = 600, height = 400, bg = "white")
 canvas.place(x = 0, y = 0)
 
 # タイマーを設定する
