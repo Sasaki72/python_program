c2c38d4e7247eaadb743f16835b89fb2e7fe80a4
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-06 17:14:19 +0900

    文字列途中

---
+++ python_study_foundation/文字列.py
@@ -0,0 +1,44 @@
+#【文字列の基礎】
+#Pythonの文字列は 「 ‘ 」（シングルクォーテーション）と「 ” 」（ダブルクォーテーション）の両方で表現出来ます。
+# print('python-izm')
+# print("python-izm")
+
+# クォート記号３つで複数行に分けて書く事も出来ます。
+# test_str = """
+# test_1
+# test_2
+# """
+# print(test_str)
+
+# 【文字列の連結】
+# 文字列の連結は「 + 」を使います。
+# test_str = 'python'
+# test_str = test_str + '-'
+# test_str = test_str + 'izm'
+# print(test_str)
+# 
+print('python-izm')
+#  
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
