7fabf16d367cde6232b107bd1260f4e893e13e28
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-07 23:42:43 +0900

    python-izu 日付・時間

--- python_study_foundation/character string.py
+++ python_study_foundation/character string.py
@@ -1,9 +1,23 @@
-#【文字列の基礎】
+# 【文字列】
+# 入門編では実行方法と基本構文を学びました。基礎編ではまず文字列について解説します。
+
+# 1 文字列の基礎
+# 2 文字列の連結
+# 3 文字列へ変換
+# 4 文字列の置換
+# 5 文字列の分割
+# 6 文字列の桁揃え
+# 7 文字列の検索
+# 8 大文字・小文字変換
+# 9 先頭・末尾の削除
+
+
+#【文字列の基礎】 
 #Pythonの文字列は 「 ‘ 」（シングルクォーテーション）と「 ” 」（ダブルクォーテーション）の両方で表現出来ます。
 # print('python-izm')
 # print("python-izm")
 #
-# 実行結果
+# 《実行結果》
 # python-izm
 # python-izm
 
@@ -15,7 +29,7 @@
 # """
 # print(test_str)
 #
-# 実行結果
+# 《実行結果》
 # test_1
 # test_2
 
@@ -29,7 +43,7 @@
 #
 # print('python-izm')
 #
-# 実行結果
+# 《実行結果》
 # python-izm
 
 
@@ -41,7 +55,7 @@
 #
 # print(test_str)
 #
-# 実行結果
+# 《実行結果》
 # 0123456789
 
 
@@ -50,7 +64,7 @@
 #
 # print(test_str)
 #
-# 実行結果
+# 《実行結果》
 # 012012012
 
 
@@ -61,7 +75,7 @@
 #
 # print(str(test_integer) + '円')
 #
-# 実行結果
+# 《実行結果》
 # 100円
 
 
@@ -72,7 +86,7 @@
 #
 # print(test_str.replace('izm', 'ism'))
 # 
-# 実行結果
+# 《実行結果》
 # python-ism
 # 
 # 
@@ -84,7 +98,7 @@
 #  
 # print(test_str.split('-'))
 # 
-# 実行結果
+# 《実行結果》
 # ['python', 'izm']
 # 
 # 
@@ -96,7 +110,7 @@
 # print(test_str.rjust(10, '0'))
 # print(test_str.rjust(10, '!'))
 # 
-# 実行結果
+# 《実行結果》
 # 0000001234
 # !!!!!!1234
 # 
@@ -107,7 +121,7 @@
 # print(test_str.zfill(10))
 # print(test_str.zfill(3))
 # 
-# 実行結果
+# 《実行結果》
 # 0000001234
 # 1234
 # 
@@ -121,7 +135,7 @@
 # print(test_str.startswith('python'))
 # print(test_str.startswith('izm'))
 # 
-# 実行結果
+# 《実行結果》
 # True
 # False
 # 
@@ -132,7 +146,7 @@
 # print('z' in test_str)
 # print('s' in test_str)
 # 
-# 実行結果
+# 《実行結果》
 # True
 # False
 # 
@@ -145,7 +159,7 @@
 # print(test_str.upper())
 # print(test_str.lower())
 # 
-# 実行結果
+# 《実行結果》
 # PYTHON-IZM.COM
 # python-izm.com
 # 
@@ -177,7 +191,7 @@
 # test_str = test_str.rstrip("com")
 # print(test_str)
 #
-#実行結果
+#《実行結果》
 # ----------------------------------
 #      python-izm.com
 # python-izm.com
---
+++ python_study_foundation/date and time.py
@@ -0,0 +1,205 @@
+# 【日付・時間】
+# プログラミングにおける日付や時間の使用頻度は高いです。言語ごとにかなり違いがあるので、調べたりする機会は多々あると思います。
+# 
+# 1 日付の取得
+# 2 日付の計算
+# 3 うるう年の判定
+# 
+# 【日付の取得】
+# 
+# 日付の取得方法は下記の通りです。標準ライブラリであるdatetimeを使用しましょう。このように標準ライブラリやサードパーティライブラリなど、特定の機能を使用したい場合はimportを用います。
+# ※str(‘a’)やint(‘1’)など、ごく一般的なプログラムにおいても高い使用頻度が想定されている一部のものは、importを行わずに使うことができます。
+# 
+# import datetime
+# 
+# 
+# today = datetime.date.today()
+# todaydetail = datetime.datetime.today()
+#  
+# # 今日の日付
+# print('----------------------------------')
+# print(today)
+# print(todaydetail)
+#  
+# # 今日に日付：それぞれの値
+# print('----------------------------------')
+# print(today.year)
+# print(today.month)
+# print(today.day)
+# print(todaydetail.year)
+# print(todaydetail.month)
+# print(todaydetail.day)
+# print(todaydetail.hour)
+# print(todaydetail.minute)
+# print(todaydetail.second)
+# print(todaydetail.microsecond)
+#  
+# # 日付のフォーマット
+# print('----------------------------------')
+# print(today.isoformat())
+# print(todaydetail.strftime("%Y/%m/%d %H:%M:%S"))
+# 
+# 《実行結果》
+# ----------------------------------
+# 2010-05-08
+# 2010-05-08 15:42:00.731000
+# ----------------------------------
+# 2010
+# 5
+# 8
+# 2010
+# 5
+# 8
+# 15
+# 42
+# 0
+# 731000
+# ----------------------------------
+# 2010-05-08
+# 2010/05/08 15:42:00
+# 
+# ※プログラムの実行時間によって結果は異なります。
+# 
+# 
+# 【日付の計算】
+# 日付の計算はtimedeltaを使用します。timedeltaは月の日数、うるう年なども気にしないでよいので、自分で計算するより多くの手間を省くことができます。
+# 
+# import datetime
+#  
+#  
+# today = datetime.datetime.today()
+#  
+# 今日の日付
+# print(today)
+#  
+# 明日の日付
+# print(today + datetime.timedelta(days=1))
+#  
+# newyear = datetime.datetime(2010, 1, 1)
+#  
+# 2010年1月1日の一週間後
+# print(newyear + datetime.timedelta(days=7))
+#  
+# 2010年1月1日から今日までの日数
+# calc = today - newyear
+#  
+# 計算結果の戻り値は「timedelta」
+# print(calc.days)
+# 
+# 《実行結果》
+# 2010-05-08 15:36:24.006000
+# 2010-05-09 15:36:24.006000
+# 2010-01-08 00:00:00
+# 127
+# 
+# ※プログラムの実行時間によって結果は異なります。
+# 
+# 
+# 【うるう年の判定】
+# その年がうるう年かどうかを判定するにはcalendar.isleapを、指定期間内に何回のうるう年があるかを取得するにはcalendar.leapdaysを使用します。
+# 
+# import calendar
+#  
+# print(calendar.isleap(2015))
+# print(calendar.isleap(2016))
+# print(calendar.isleap(2017))
+#  
+# print(calendar.leapdays(2010, 2020))
+# 
+# 《実行結果》
+# False
+# True
+# False
+# 2
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
