97f17edd956dc9096b4e88f766945044dedd6c5d
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-09 14:22:08 +0900

    Python-izm入門終了

--- Python-izm_foundation/basic practice.py
+++ Python-izm_foundation/basic practice.py
@@ -1,23 +1,5 @@
-def exception_test(value_1, value_2):
+print('あなたの名前を教えてください。')
 
-    print('＝＝＝＝計算開始＝＝＝＝')
+your_name = input('>> ')
 
-    result = 0
-
-    try:
-        result = value_1 + value_2
-    except:
-        print('計算出来ませんでした！')
-        raise
-    finally:
-        print('計算終了')
-
-    return result
-
-
-try:
-    print(exception_test(100, 100))
-    print(exception_test(200, 200))
-    print(exception_test(300, '300'))
-except:
-    print('エラーを受け取りました')
+print(your_name)
