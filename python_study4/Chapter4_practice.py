ba3c87e1b4bdc39ab3c447d2ff32ecac900effdf
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-12 15:51:05 +0900

    Chapter4-7終了

--- python_study4/Chapter4_practice.py
+++ python_study4/Chapter4_practice.py
@@ -1,9 +1,2 @@
-#coding:utf-8
-def tashizan(a, b):
-    total = 0
-    for i in range(a, b + 1):
-        total = total + i
-    return total
-
-c = tashizan(1, 5)
-print(c)
+import calendar
+print(calendar.month(2020,7))
