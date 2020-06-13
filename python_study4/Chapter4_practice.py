8223b7afe16f18aa273e8e8b3d8c57fc8a556ca5
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-13 16:35:32 +0900

    Chapter4まとめ途中

--- python_study4/Chapter4_practice.py
+++ python_study4/Chapter4_practice.py
@@ -1,2 +1,27 @@
-import calendar
-print(calendar.month(2020,7))
+# coding:utf-8
+# for a in range(101):
+#     if (a % 12 == 0):
+#       print("○")
+#     elif (a % 4 == 0):
+#       print("△")
+#     elif if (a % 2 == 0):
+#       print("×")
+#     else:
+#       print("☆")
+
+# total = 0
+# a = 1
+# while total <= 50:      # 「total <= 50」 50以下の時に繰り返す
+#     total = total + a
+#     a = a + 1
+# print(total)
+
+# total = 0
+# a = 1
+# while True:             #「while True」 永遠に切り返す
+#     total = total + a
+#     a = a + 1
+#     if total > 50:      # 50を超えたら
+#       break             # 繰返しをやめる 
+# print(total)
+
