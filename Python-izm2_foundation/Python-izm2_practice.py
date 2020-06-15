85162d4fb2d87479a036400b5bf3276be780e8a1
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-15 23:24:24 +0900

    文字列確認作業

--- Python-izm2_foundation/Python-izm2_practice.py
+++ Python-izm2_foundation/Python-izm2_practice.py
@@ -39,3 +39,29 @@ for value in test_list_1:
 for value_1, value_2, value_3 in test_list_1:
     print(value_1, value_2, value_3)
     
+# 関数とメソッド 
+# 関数
+def test_func():
+    print('call test_func')
+  
+  
+class TestClass:
+    # メソッド
+    def test_method():
+        print('call test_method')
+ 
+print('------------------------')
+print(test_func)
+print(TestClass.test_method)
+ 
+print('------------------------')
+print(type(test_func))
+print(type(TestClass.test_method))
+ 
+print('------------------------')
+t = TestClass()
+print(test_func)
+print(t.test_method)
+
+
+
