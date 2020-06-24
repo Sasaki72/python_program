78874e8756694bd1ced47b952b484565de402994
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-24 23:51:34 +0900

    復習

--- for_practice/practice1.py
+++ for_practice/practice1.py
@@ -3,20 +3,19 @@
 apple_price = 200
 money = 1000
 
-input_count = input("購入するりんごの数を入力してください")
+input_count = input("購入するりんごの数を入れてください")
 count = int(input_count)
-total_price = apple_price * count
+total_price = count * apple_price
 
 print("購入するりんごの数は" + str(count) + "個です")
-print("支払いは" + str(total_price) + "円です")
+print("支払い金額は" + str(total_price) + "円です")
 
 if money > total_price:
     print("購入するりんごの数は" + str(count) + "個です")
-    print("残金は" + str(money - total_price) + "円です")
+    print("支払い金額は" + str(total_price) + "円です")
 elif money == total_price:
     print("購入するりんごの数は" + str(count) + "個です")
-    print("財布は空になりました")
+    print("財布が空になりました")
 else:
-    print("金額が足りません")
-    print("りんごを買えませんでした")
-
+    print("お金が足りません")
+    print("購入できませんでした")
