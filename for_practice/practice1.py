91ffc8dff1264a9827ab97f6ef9f923a2d8d06a2
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-06-28 23:39:50 +0900

    ___init__復習

--- for_practice/practice1.py
+++ for_practice/practice1.py
@@ -1,26 +1,20 @@
-# 関数validateを定義してください
-def validate(hand):
-    # handの値によって条件分岐してください
-    if hand < 0 or hand > 2:
-        return False
-    else:
-        return True
-    
-def print_hand(hand, name='ゲスト'):
-    hands = ['グー', 'チョキ', 'パー']
-    print(name + 'は' + hands[hand] + 'を出しました')
+class MenuItem:
+    def __init__(self):
+        print("MenuItemクラスのインスタンスが生成されました！")
 
-print('じゃんけんをはじめます')
-player_name = input('名前を入力してください：')
-print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
-player_hand = int(input('数字で入力してください：'))
+    def info(self):
+        return self.name + ': ¥' + str(self.price)
 
-# 関数validateの戻り値がTrueの場合、以下のif~else文が実行されるようにしてください
-if validate(player_hand):
-    if player_name == '':
-        print_hand(player_hand)
-    else:
-        print_hand(player_hand, player_name)
-# 関数validateの戻り値がFalseの場合「正しい数値を入力してください」と出力してください
-else:
-    print("正しい数値を入力してください")
+    def get_total_price(self, count):
+        total_price = self.price * count
+        return total_price
+
+
+menu_item1 = MenuItem()
+menu_item1.name = 'サンドイッチ'
+menu_item1.price = 500
+
+print(menu_item1.info())
+
+result = menu_item1.get_total_price(4)
+print('合計は' + str(result) + '円です')
