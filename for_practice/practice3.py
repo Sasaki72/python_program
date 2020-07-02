e9ed4073ac576005942e2fcab12fc9b6d2f066dc
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-07-02 23:09:37 +0900

    継承確認

--- for_practice/practice3.py
+++
@@ -1,6 +0,0 @@
-# fromとimportを用いて、MenuItemクラスを読み込んでください
-from practice2 import MenuItem
-
-# MenuItemクラスを継承して、Foodクラスを定義してください
-class Food(MenuItem):
-    pass
--- for_practice/practice4.py
+++
@@ -1,6 +0,0 @@
-# fromとimportを用いて、MenuItemクラスを読み込んでください
-from practice2 import MenuItem
-
-# MenuItemクラスを継承して、Drinkクラスを定義してください
-class Drink(MenuItem):
-    pass
