c1234245058df2d8db64aebc46a3b058684b4841
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-07-04 23:22:55 +0900

    paiza練習

---
+++ for_practice/a.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+a = input()
+li = []
+for i in range(int(a)):
+    b = input()
+    li.append(b)
+print("Hello " + ",".join(li) + ".")
+
+
+a = input()
+li = []
+for i in range(int(a))
+    b = input()
+    li = append(b)
+print(a)
+
+test = input()
+li = []
+for i in range(1,10):
+    a = i * int(test)
+    li.append(str(a))
+print(" ".join(li))
