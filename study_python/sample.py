4b1c146fa7d3448e159c21d76b1057c853aced6a
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-07-18 22:52:20 +0900

    study

---
+++ study_python/sample.py
@@ -0,0 +1,36 @@
+.split = 文字列を指定した区切り文字で分割してリストとして取得する
+"My First Album".split()
+# ['My', 'First', 'Album']
+
+list
+test_list_1 = ['python', 'izm', 'com']
+
+append = リストに新しい要素を追加する方法です
+mylist = ["A", "B", "C"]
+# mylist.append("D")
+# print(mylist)
+# --> ["A", "B", "C", "D"]
+
+.join = リストなどに要素として格納されている文字列を指定した区切り文字で連結する
+ "".join(["Apple", "Orange", "Lemon"])
+# 'AppleOrangeLemon'
+# >>> ",".join(("Blue", "Red", "Green"))
+# 'Blue,Red,Green'
+
+range = 引数に指定した開始数から終了数までの連続した数値を要素として持つ
+range(5)
+# --> 0 1 2 3 4
+
+if
+elif
+else
+for
+max
+min
+str
+int
+「map」
+.find
+abs
+.upper
+インデックス、スライス
