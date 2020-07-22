a9d0cc6f564db5972dad5979fad8e685a7d7d340
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-07-22 17:43:22 +0900

    名前変更

---
+++ study_python/D003.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+input_line = input()
+input_list = input_line.split()
+int_list = []
+for i in input_list:
+    int_list.append(int(i))
+if int_list[0] == int_list[1]:
+    print("eq")
+else:
+    print(max(int_list))
